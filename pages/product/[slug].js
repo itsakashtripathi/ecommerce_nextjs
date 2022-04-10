b2a9db7f87d04d9559d615996ae15dfb0fc5ab2d
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-10 23:52:04 +0530

    Rendering Products Dynamically Using Router

---
+++ pages/product/[slug].js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+import data from '../../utils/data';
+
+function ProductScreen() {
+    const router = useRouter();
+    const {slug} = router.query;
+    const product = data.products.find( a => a.slug === slug);
+    if (!product) {
+        return (<div>Product Not Found</div>)
+    }
+  return (
+    <div>
+        <h1>{product.name}</h1>
+    </div>
+  )
+}
+
+export default ProductScreen
