5ce81cc1727bc080eb39906f49772574f0059c52
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-10 21:28:54 +0530

    Added Styles

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
-import {AppBar, Toolbar, Typography, Container} from '@mui/material';
+import {AppBar, Toolbar, Typography, Container} from '@material-ui/core';
+import useStyles from '../utils/styles';
 
 function Layout({children}) {
+    const classes = useStyles();
   return (
     <div>
         <Head>
             <title>Next Ecommerce</title>
         </Head>
-        <AppBar position="static">
+        <AppBar position="static" className={classes.navbar}>
             <Toolbar>
                 <Typography>Amazona</Typography>
             </Toolbar>
         </AppBar>
-        <Container>
+        <Container className={classes.main}>
             {children}
         </Container>
-        <footer>
+        <footer className={classes.footer}>
             <Typography>
                 All Rights Reserved. Akash Tripathi
             </Typography>
