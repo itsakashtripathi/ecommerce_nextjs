777d9deb696d053dd3dd857d860ec2daa61498f0
itsakashtripathi <98697978+itsakashtripathi@users.noreply.github.com>
2022-04-10 23:26:07 +0530

    Added Links To Navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
-import {AppBar, Toolbar, Typography, Container} from '@material-ui/core';
+import NextLink from 'next/link';
+import {AppBar, Toolbar, Typography, Container, Link} from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 function Layout({children}) {
@@ -12,7 +13,20 @@ function Layout({children}) {
         </Head>
         <AppBar position="static" className={classes.navbar}>
             <Toolbar>
-                <Typography>Amazona</Typography>
+                <NextLink href='/' passHref>
+                    <Link>
+                        <Typography className={classes.brand}>Amazona</Typography>
+                    </Link>
+                </NextLink>
+                <div className={classes.grow}></div>
+                <div>
+                    <NextLink href='/cart' passHref>
+                        <Link>Cart</Link>
+                    </NextLink>
+                    <NextLink href='/login' passHref>
+                        <Link>Login</Link>
+                    </NextLink>
+                </div>
             </Toolbar>
         </AppBar>
         <Container className={classes.main}>
